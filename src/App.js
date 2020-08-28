7ee8d83e5cc3741594003bf0c6e83c4f8c8b26c0
Jon Haase <haase1020@gmail.com>
2020-08-28 19:13:59 -0400

    almost done with basic functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
@@ -8,17 +13,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Clean the kitchen',
         completed: false,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Take out the garbage',
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Clean the bathrooms',
         completed: false,
       },
@@ -44,17 +49,40 @@ class App extends Component {
     });
   };
 
+  // Add
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
-    console.log(this.state.todos);
     return (
-      <div className='App'>
-        <Header />
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
-      </div>
+      <Router>
+        <div className='App'>
+          <div className='container'>
+            <Header />
+            <Route
+              exact
+              path='/'
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path='/about' component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is my help out list. I want my family to help out more.</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
